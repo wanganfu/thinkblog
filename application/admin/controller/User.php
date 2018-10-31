b0f836772309baa42c3cf22619ed99f8f54da779
annon <1435427137@qq.com>
2018-10-31 18:07:24 +0800

    test

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -3,7 +3,6 @@
 namespace app\admin\controller;
 
 use think\Controller;
-use think\Request;
 
 class User extends Controller
 {
@@ -12,5 +11,9 @@ class User extends Controller
      */
     public function index() {
         return 'App\admin\controller\User';
+    }
+
+    public function test() {
+        return 'test';
     }
 }
